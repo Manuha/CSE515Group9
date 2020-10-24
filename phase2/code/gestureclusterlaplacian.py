cc078c270533c62163be0df1cff42410637982b5
gretino <fuqiqiqiang@gmail.com>
2020-10-23 22:27:07 -0700

    Added not fully implemented laplacian spectral clustering

--- phase2/code/gestureclusterlaplacian.py
+++ phase2/code/gestureclusterlaplacian.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 import networkx
 import random
+import matplotlib.pyplot as plt
 from scipy.sparse.linalg import eigs
 
 # folder = sys.argv[1]
@@ -26,39 +27,55 @@ from scipy.sparse.linalg import eigs
 # else:
 #     print('wrong clustering option')
 #
-# testmatrix = np.zeros((10, 10))
-# for i in range(len(testmatrix) // 2):
-#     for j in range(len(testmatrix) // 2):
-#         testmatrix[i, j] = 1
-#         testmatrix[j, i] = 1
-# for i in range(len(testmatrix) // 2, len(testmatrix)):
-#     for j in range(len(testmatrix) // 2, len(testmatrix)):
-#         testmatrix[i, j] = 1
-#         testmatrix[j, i] = 1
+testmatrix = np.zeros((10, 10))
+for i in range(len(testmatrix) // 2):
+    for j in range(len(testmatrix) // 2):
+        testmatrix[i, j] = 1
+        testmatrix[j, i] = 1
+for i in range(len(testmatrix) // 2, len(testmatrix)):
+    for j in range(len(testmatrix) // 2, len(testmatrix)):
+        testmatrix[i, j] = 1
+        testmatrix[j, i] = 1
 
 
-#print(testmatrix)
+# print(testmatrix)
 
 
-def gesturecluster(matrix, k=2):
+def gesturecluster(matrix, k, knear=2):
     if not matrix.shape[0] == matrix.shape[1]:
         print('matrix is not a square')
     G = networkx.Graph()
     for i in range(len(matrix)):
         G.add_node(str(i))
     for i in range(len(matrix)):
-        #subvector = matrix[i, :]
-        #knearest = np.argpartition(subvector, -knear)[-knear - 1:-1]  # k nearest
-        for j in range(len(matrix)):
-            if matrix[i][j] > 0:  # graph weight need to be positive
-                G.add_edge(str(i), str(j), weight=matrix[i][j])  # build k-nearest graph
+        subvector = matrix[i, :]
+        print(subvector)
+        nearestranking = np.argsort(subvector, )[::-1]
+        knearest = nearestranking[:knear]  # k nearest
+        print(knearest)
+        for j in knearest:
+            if subvector[j] > 0:  # graph weight need to be positive
+                G.add_edge(str(i), str(j), weight=subvector[j])  # build k-nearest graph
+    # pos = networkx.spring_layout(G)
+    # networkx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+    # networkx.draw_networkx_nodes(G, pos, node_size=400)
+    # networkx.draw_networkx_edges(G, pos, edgelist=G.edges, width=4)
+    # plt.show()
+
     normalaplacian = networkx.normalized_laplacian_matrix(G)
-    kvals, kvecs = eigs(normalaplacian, k, which = 'SM')
-    V = abs(np.mat(kvecs))  # k*n matrix for cluster
+    kvals, kvecs = eigs(normalaplacian, k)
+    V = np.mat(kvecs).real  # k*n matrix for cluster
+    print(V)
+    U = np.zeros(shape=V.shape)
+    for n in range(0, U.shape[0]):
+        temp = np.sum(np.power(V[n, :], 2)) ** (1 / 2)
+        for k in range(0, U.shape[1]):
+            U[n, k] = V[n, k] / temp
+    print(U)
     kmeans = KMeans(n_clusters=k, random_state=0).fit(V)
     clusterresult = kmeans.labels_
     print(clusterresult)
     return clusterresult
 
 
-# gesturecluster(testmatrix, 2)  # for debugging only, no real meaning
+gesturecluster(testmatrix, 2)
