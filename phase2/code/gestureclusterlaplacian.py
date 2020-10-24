a1cd2549f9fc140e30f9d174c10787733f5eb92c
gretino <fuqiqiqiang@gmail.com>
2020-10-23 22:49:54 -0700

    attempted to fix

--- phase2/code/gestureclusterlaplacian.py
+++ phase2/code/gestureclusterlaplacian.py
@@ -41,7 +41,7 @@ for i in range(len(testmatrix) // 2, len(testmatrix)):
 # print(testmatrix)
 
 
-def gesturecluster(matrix, k, knear=2):
+def gesturecluster(matrix, k, knear=3):
     if not matrix.shape[0] == matrix.shape[1]:
         print('matrix is not a square')
     G = networkx.Graph()
@@ -49,10 +49,10 @@ def gesturecluster(matrix, k, knear=2):
         G.add_node(str(i))
     for i in range(len(matrix)):
         subvector = matrix[i, :]
-        print(subvector)
+        #print(subvector)
         nearestranking = np.argsort(subvector, )[::-1]
         knearest = nearestranking[:knear]  # k nearest
-        print(knearest)
+        #print(knearest)
         for j in knearest:
             if subvector[j] > 0:  # graph weight need to be positive
                 G.add_edge(str(i), str(j), weight=subvector[j])  # build k-nearest graph
@@ -65,14 +65,17 @@ def gesturecluster(matrix, k, knear=2):
     normalaplacian = networkx.normalized_laplacian_matrix(G)
     kvals, kvecs = eigs(normalaplacian, k)
     V = np.mat(kvecs).real  # k*n matrix for cluster
-    print(V)
+    #print(V)
     U = np.zeros(shape=V.shape)
     for n in range(0, U.shape[0]):
         temp = np.sum(np.power(V[n, :], 2)) ** (1 / 2)
-        for k in range(0, U.shape[1]):
-            U[n, k] = V[n, k] / temp
+        for k1 in range(0, U.shape[1]):
+            U[n, k1] = V[n, k1] / temp
+    for i in range(0, U.shape[0]):
+        if U[i, 0] < 0:
+            U[i, :] = -U[i, :]
     print(U)
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(V)
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(U)
     clusterresult = kmeans.labels_
     print(clusterresult)
     return clusterresult
